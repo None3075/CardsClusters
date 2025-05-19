0d5ace25e0362d0396ef0f7e33be2b3eba14fc5e
Martin <martin.f@opendeusto.es>
2025-05-19 20:09:44 +0200

    queries y fag de corriente de aire

---
+++ Airflow_dags/dag_queries.py
@@ -0,0 +1,96 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+### Tutorial Documentation
+Documentation that goes along with the Airflow tutorial located
+[here](https://airflow.apache.org/tutorial.html)
+"""
+
+from __future__ import annotations
+
+# [START tutorial]
+# [START import_module]
+import textwrap
+from datetime import datetime, timedelta
+
+# Operators; we need this to operate!
+from airflow.providers.standard.operators.bash import BashOperator
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.sdk import DAG
+
+# [END import_module]
+
+
+# [START instantiate_dag]
+with DAG(
+    "dag_queries",
+    # [START default_args]
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        "run_as_user": "ec2-user",
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'on_skipped_callback': another_function, #or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+    # [END default_args]
+    description="A simple tutorial DAG",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=["Entrega"],
+) as dag:
+    # [END instantiate_dag]
+
+    # t1, t2 and t3 are examples of tasks created by instantiating operators
+    # [START basic_task]
+    t1 = BashOperator(
+        task_id="Remove_old_hdfs_output_query1",
+        bash_command="hdfs dfs -rm -r /user/ec2-user/output/query1",
+    )
+    
+    
+    t6 = BashOperator(
+        task_id="query1",
+        bash_command="spark-submit --master yarn /home/ec2-user/SparkQueries/query1.py",
+    )
+    
+    t11 = BashOperator(
+        task_id="Retrieve_results_to_local_query1",
+        bash_command="hdfs dfs -get /user/ec2-user/output/query1 /home/ec2-user/SparkQueries/Results",
+    )
+        
+    
+    # set dependencies
+    t1 >> t6 >> t11
+    
+
+# [END tutorial]
